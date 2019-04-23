5049a7cdc6041a931b11576cfcd89119ee98e384
Chris Westbrook <westbchris@gmail.com>
2019-04-23 17:15:08 -0400

    Got validation working.

--- src/requestContainer.js
+++ src/requestContainer.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import moment from 'moment';
 import RequestForm from './requestForm';
 import { FirebaseContext } from './firebase';
 //import { validateText, validateNumber, validateDate } from './validate';
@@ -6,15 +7,46 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 
 const RequestContainer = () => {
 	const [request, setRequest] = useState({ numDays: '0', startDate: '', endDate: '', description: '', type: '' });
+	const [touched, setTouched] = useState({ startDate: false, endDate: false, description: false, type: false });
+	const calcDiff = (startDate, endDate) => {
+		let dStartDate = moment(startDate, 'M/D/YYYY', true);
+		let dEndDate = moment(endDate, 'M/D/YYYY', true);
+		if (dStartDate.isValid() && dEndDate.isValid() && dEndDate >= dStartDate) {
+			setRequest({ ...request, numDays: dEndDate.businessDiff(dStartDate) + 1 });
+		} else {
+			setRequest({ ...request, numDays: 0 });
+		}
+	};
+	const { startDate, endDate } = request;
+	useEffect(() => {
+		calcDiff(startDate, endDate);
+	}, [startDate, endDate]);
+	const handleBlur = e => {
+		const { name } = e.target;
+		setTouched({ ...touched, [name]: true });
+	};
+	const setAllTouched = () => {
+		setTouched({ description: true, type: true, startDate: true, endDate: true });
+	};
 	const firebase = useContext(FirebaseContext);
 	const { init, user } = useAuthState(firebase.auth);
-	const handleChange = (name, value) => {
+	const handleChange = e => {
+		const { name, value } = e.target;
 		setRequest({ ...request, [name]: value });
 	};
 	const addRequest = request => {
 		request.uid = user.uid;
 		const q = firebase.db.collection('requests').add(request);
 	};
-	return <RequestForm request={request} handleChange={handleChange} onAdd={addRequest} />;
+	return (
+		<RequestForm
+			request={request}
+			touched={touched}
+			handleChange={handleChange}
+			onAdd={addRequest}
+			handleBlur={handleBlur}
+			setAllTouched={setAllTouched}
+		/>
+	);
 };
 export default RequestContainer;
--- src/requestForm.js
+++ src/requestForm.js
@@ -1,61 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
-import { validateDate, validateNumber, validateText } from './validate';
-const RequestForm = ({ request, handleChange, onAdd }) => {
-	const { type, startDate, endDate, description } = request;
+//import { validateDate, validateNumber, validateText } from './validate';
+import moment from 'moment';
+import * as businessDiff from 'moment-business-days';
+const RequestForm = ({ request, touched, handleChange, onAdd, handleBlur, setAllTouched }) => {
+	useEffect(() => {
+		validateForm(request);
+	}, [request, touched]);
+	const { type, startDate, endDate, description, numDays } = request;
 	const [startDateMsg, setStartDateMsg] = useState('');
 	const [endDateMsg, setEndDateMsg] = useState('');
 	const [typeMsg, setTypeMsg] = useState('');
 	const [descriptionMsg, setDescriptionMsg] = useState('');
+
+	const validateForm = request => {
+		const { startDate, endDate, description, type } = request;
+
+		setTypeMsg(type === '' ? 'You must select a type' : '');
+		setDescriptionMsg(description === '' ? 'You must enter a description' : '');
+		setStartDateMsg(!moment(startDate, 'M/D/YYYY', true).isValid() ? 'You must enter a valid date' : '');
+		setEndDateMsg(!moment(endDate, 'M/D/YYYY', true).isValid() ? 'You must enter a valid date' : '');
+		let dStartDate = moment(startDate, 'M/D/YYYY', true);
+		let dENdDate = moment(endDate, 'M/D/YYYY', true);
+		if (startDate !== '' && endDate !== '' && dStartDate.isValid() && dENdDate.isValid()) {
+			console.log('Is valid dae');
+			if (dStartDate > dENdDate) {
+				setStartDateMsg('Your start date must be earlier than your end date.');
+				setEndDateMsg('Your start date must be earlier than your end date.');
+			} else {
+				setStartDateMsg('');
+				setEndDateMsg('');
+			}
+		}
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		setAllTouched();
+		if (typeMsg || startDateMsg || descriptionMsg || endDateMsg) {
+			console.log('invalid');
+		} else console.log('valid');
+	};
+
 	return (
 		<div>
-			<Form>
+			<Form onSubmit={handleSubmit}>
 				<Form.Group controlId="type">
-					<Form.Control
-						as="select"
-						value={type}
-						onChange={e => {
-							handleChange('type', e.target.value);
-						}}
-					>
+					<Form.Control as="select" onBlur={handleBlur} value={type} onChange={handleChange} name="type">
 						<option value="">?</option>
 						<option>Personal</option>
 						<option>Sick</option>
 						<option>Vacation</option>
 					</Form.Control>
-					{typeMsg && <Form.Control.Feedback type="invalid">{typeMsg}</Form.Control.Feedback>}
+					{typeMsg && touched['type'] && (
+						<Form.Control.Feedback type="invalid">{typeMsg}</Form.Control.Feedback>
+					)}
 				</Form.Group>
 				<Form.Group controlId="startDate">
 					<Form.Label>Start Date</Form.Label>
 					<Form.Control
 						type="text"
 						name="startDate"
+						onBlur={handleBlur}
 						value={startDate}
-						onChange={e => {
-							const { name, value } = e.target;
-							handleChange(name, value);
-							setStartDateMsg(validateDate(value, 'You must enter a valid date.'));
-						}}
+						onChange={handleChange}
 					/>
-					{startDateMsg && <Form.Control.Feedback type="invalid">{startDateMsg}</Form.Control.Feedback>}
+					{startDateMsg && touched['startDate'] && (
+						<Form.Control.Feedback type="invalid">{startDateMsg}</Form.Control.Feedback>
+					)}
 				</Form.Group>
 
 				<Form.Group controlId="endDate">
 					<Form.Label>End Date</Form.Label>
 					<Form.Control
+						onBlur={handleBlur}
 						type="text"
 						name="endDate"
-						onChange={e => {
-							const { name, value } = e.target;
-							handleChange(name, value);
-							setEndDateMsg(validateDate(value, 'You must enter a valid date.'));
-						}}
+						onChange={handleChange}
 						value={endDate}
 					/>
-					{endDateMsg && <Form.Control.Feedback type="invalid">{endDateMsg}</Form.Control.Feedback>}
+					{endDateMsg && touched['endDate'] && (
+						<Form.Control.Feedback type="invalid">{endDateMsg}</Form.Control.Feedback>
+					)}
 				</Form.Group>
+				<Form.Group controlId="description">
+					<Form.Label>Description</Form.Label>
+					<Form.Control
+						onBlur={handleBlur}
+						name="description"
+						value={description}
+						type="text"
+						onChange={handleChange}
+					/>
+					{descriptionMsg && touched['description'] && (
+						<Form.Control.Feedback>{descriptionMsg}</Form.Control.Feedback>
+					)}
+				</Form.Group>
+				<button type="submit">Submit</button>
 			</Form>
-			{JSON.stringify(request)}
+
+			{numDays > 0 && <div>Your vacation is {numDays} days long.</div>}
 		</div>
 	);
 };
