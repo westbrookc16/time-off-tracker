8f0d6a18709376f54430f190406c368e700ec951
Chris Westbrook <westbchris@gmail.com>
2019-04-25 19:34:36 -0400

    mend

---
+++ src/ViewRequestsContainer.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext, useEffect } from 'react';
+import ReqList from './reqlist';
+import { FirebaseContext } from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const ViewRequestContainer = () => {
+	const firebase = useContext(FirebaseContext);
+	//eslint-disable-next-line
+	const { init, user } = useAuthState(firebase.auth);
+
+	const [reqList, setReqList] = useState([]);
+
+	const [year, setYear] = useState('2019');
+
+	useEffect(() => {
+		console.log(`year=${year}`);
+		firebase.db
+			.collection('requests')
+			.where('uid', '==', user ? user.uid : '')
+			.where('year', '==', parseInt(year))
+			.orderBy('startDate')
+			.get()
+			.then(data => {
+				//console.log(data);
+				const list = [];
+				data.forEach(doc => {
+					let req = {};
+					req['id'] = doc.id;
+					req = { ...req, ...doc.data() };
+					list.push(req);
+				});
+
+				setReqList(list);
+			})
+			.catch(e => {
+				console.log(e);
+			});
+	}, [user, year]);
+
+	return (
+		<div>
+			<label htmlFor="year">Year</label>
+			<select
+				id="year"
+				name="year"
+				onChange={e => {
+					setYear(e.target.value);
+				}}
+				value={year}
+			>
+				<option>2019</option>
+				<option>2020</option>
+				<option>2021</option>
+			</select>
+			<br />
+			<ReqList reqList={reqList} />
+		</div>
+	);
+};
+export default ViewRequestContainer;
---
+++ src/reqlist.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import moment from 'moment';
+const ReqList = ({ reqList }) => {
+	let trs = [];
+	//if (!reqList) return <div />;
+	trs = reqList.map(i => {
+		const data = i;
+
+		return (
+			<tr key={i.id}>
+				<td>{data.description}</td>
+				<td>{moment(data.startDate.toDate()).format('M/D/YYYY')}</td>
+				<td>{moment(data.endDate.toDate()).format('M/D/YYYY')}</td>
+				<td>{data.type}</td>
+			</tr>
+		);
+	});
+	let totals = { totalSick: 0, totalVacation: 0, totalPersonal: 0 };
+	reqList.forEach(i => {
+		totals[`total${i.type}`] += parseInt(i.numDays);
+	});
+	return (
+		<div>
+			<h1>Requests</h1>
+			<table>
+				<thead>
+					<tr>
+						<td>Description</td>
+						<td>Start Date</td>
+						<td>End Date</td>
+						<td>Type</td>
+					</tr>
+				</thead>
+				<tbody>{trs}</tbody>
+			</table>
+			<h1>Totals</h1>
+			<table>
+				<tr>
+					<td>Sick</td>
+					<td>{totals.totalSick}</td>
+				</tr>
+				<tr>
+					<td>Personal</td>
+					<td>{totals.totalPersonal}</td>
+				</tr>
+				<tr>
+					<td>Vacation</td>
+					<td>{totals.totalVacation}</td>
+				</tr>
+			</table>
+		</div>
+	);
+};
+export default ReqList;
